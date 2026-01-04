ef0bb3c0b4a2e87848ae5238b1a1d1b330dd7927
pchatain <pchatain@stanford.edu>
2026-01-04 14:43:00 -0800

    stop overwriting data in backend as that was an old approach and caused bugs

--- packages/backend/run.sh
+++ packages/backend/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Copy results from main directory into backend data folder
-rm -rf data/results
-mkdir -p data
-cp -r ../../results data/results
-
-echo "Copied results to data/results"
-
 uv run --env-file ../../.env uvicorn moralbench_api.main:app --reload --host 127.0.0.1 --port 8000
