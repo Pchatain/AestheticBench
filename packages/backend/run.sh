f745bae561dc81ff69e26cf8f6fb66c979616e64
pchatain <pchatain@stanford.edu>
2026-01-04 18:02:49 -0800

    Add MORALBENCH_RESULTS_DIR env var to run.sh

--- packages/backend/run.sh
+++ packages/backend/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+# Set results directory (computed from script location)
+export MORALBENCH_RESULTS_DIR="$(cd ../.. && pwd)/results"
+echo "Results directory: $MORALBENCH_RESULTS_DIR"
+
 uv run --env-file ../../.env uvicorn moralbench_api.main:app --reload --host 127.0.0.1 --port 8000
